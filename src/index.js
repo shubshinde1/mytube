ab1064f53e018b877fda76d24d69ac2906245149
shushinde111@gmail.com <shushinde111@gmail.com>
2024-06-07 13:50:46 +0530

    middleware added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
-import connectDB from "./db/index.js ";
+import connectDB from "./db/index.js";
 import { app } from "./app.js";
-
-dotenv.config({ path: "./env" });
+dotenv.config({
+  path: "./.env",
+});
 
 connectDB()
   .then(() => {
-    app.litsen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
     });
   })
   .catch((err) => {
-    console.log("Mongo DB Connection Faied", err);
+    console.log("MONGO db connection failed !!! ", err);
   });
 
 // <--this is IFFE(imediate invoke function expression) Function to excute function when index file loaded -->
